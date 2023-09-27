bb3fae9c8a437d77aa3ed6b366ca6a15e4972de8
Xayiide <wickedcheerio@protonmail.com>
2023-09-27 19:17:41 +0200

    Se arregla main con el código de ejemplo de GLFW. Se empieza a realizar la librería pixel

--- src/main.c
+++ src/main.c
@@ -1,69 +1,141 @@
+#include <GL/glew.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-#include <GL/glew.h>
 
-#include "linmath.h"
 
-#include <stdio.h>
+#include <stdlib.h>  /* exit    */
+#include <stdio.h>   /* fprintf */
 
-void error_callback(int error, const char *description)
+#include "linmath.h" /* mat4x4  */
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
+
+static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
 }
 
-static void key_callback(GLFWwindow *window, 
-        int key, int scancode, int action, int mods)
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-int main()
+int main(void)
 {
-    GLFWwindow *window;
-    printf("[+] Pruebas GLFW\n");
+    GLFWwindow* window;
+    GLuint      vertex_buffer, vertex_shader, fragment_shader, program;
+    GLint       mvp_location, vpos_location, vcol_location;
 
-    if (!glfwInit()) {
-        fprintf(stderr, "[-] La inicialización de GLFW falló.\n");
-        return -1;
-    }
-    else {
-        printf("[+] Se ha inicializado bien GLFW.\n");
-        glfwTerminate();
-        printf("[+] GLFW terminado.\n");
-    }
-
-    /* Error callback */
     glfwSetErrorCallback(error_callback);
 
-    /* Crea una ventana */
-    window = glfwCreateWindow(640, 480, "Título", NULL, NULL);
-    if (!window) {
-        fprintf(stderr, "[-] La creación de una ventana falló.\n");
-        glfwTerminate();
-        return -1;
+    if (glfwInit() == GL_FALSE) {
+        fprintf(stderr, "[-] Error inicializando GLFW.\n");
+        exit(EXIT_FAILURE);
     }
-    
-    /* Crea contexto OpenGL */
+
+    window = glfwCreateWindow(640, 480, "Triangulito", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "[-] Error creando ventana.\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(window, key_callback);
+
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-    /* Contexto OpenGL y ajustes */
     glewExperimental = GL_TRUE;
     if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "[-] La inicialización de GLEW falló.\n");
-        return -1;
+        fprintf(stderr, "Error al inicializar GLEW.\n");
+        exit(EXIT_FAILURE);
     }
 
-    while (!glfwWindowShouldClose(window)) {
-        
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glCompileShader(vertex_shader);
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glCompileShader(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    mvp_location = glGetUniformLocation(program, "MVP");
+    vpos_location = glGetAttribLocation(program, "vPos");
+    vcol_location = glGetAttribLocation(program, "vCol");
+
+    glEnableVertexAttribArray(vpos_location);
+    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(vertices[0]), (void *) 0);
+    glEnableVertexAttribArray(vcol_location);
+    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(vertices[0]), (void *) (sizeof(float) * 2));
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float  ratio;
+        int    width, height;
+        mat4x4 m, p, mvp;
+
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        mat4x4_identity(m);
+        mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+        mat4x4_mul(mvp, p, m);
+
+        glUseProgram(program);
+        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *) mvp);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
+
     glfwTerminate();
-
-    printf("[+] Ventana destruida y GLFW terminado.\n");
-
-    return 0;
+    exit(EXIT_SUCCESS);
 }
-
--- src/pixel.c
+++ src/pixel.c
@@ -1,9 +1,61 @@
 #include "pixel.h"
 
-#include <stdio.h>
+#include <GL/glew.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 
-void px_init(void)
+#include <stdio.h>  /* printf */
+#include <stdlib.h> /* exit   */
+
+
+static void error_callback(int err, const char *desc);
+static void tecla_callback(GLFWwindow *ventana, int tecla, int scancode,
+                           int accion, int mods);
+
+
+void px_init(int ancho, int alto, const char *titulo)
 {
     printf("[px] Inicializando...\n");
+
+    GLFWwindow *ventana;
+
+    glfwSetErrorCallback(error_callback);
+
+    if (glfwInit() == GL_FALSE) {
+        fprintf(stderr, "[px] Error inicializando GLFW.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ventana = glfwCreateWindow(ancho, alto, titulo, NULL, NULL);
+    if (ventana == NULL) {
+        fprintf(stderr, "[px] Error creando la ventana %s.\n", titulo);
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSetKeyCallback(ventana, tecla_callback);
+    glfwMakeContextCurrent(ventana);
+    glfwSwapInterval(1);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "[px] Error al inicializar GLEW.\n");
+        exit(EXIT_FAILURE);
+    }
+
+
 }
 
+
+static void error_callback(int err, const char *desc)
+{
+    fprintf(stderr, "[px] Error %d: %s\n", err, desc);
+}
+
+
+static void keybd_callback(GLFWwindow *ventana, int tecla, int scancode,
+                           int accion, int mods)
+{
+    if (tecla == GLFW_KEY_ESCAPE && accion == GLFW_PRESS)
+        glfwSetWindowShouldClose(ventana, GLFW_TRUE);
+}
